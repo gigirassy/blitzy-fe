ec6f7994476fd7ec3b6c5a0adf21b7a17419fb15
nune <145225213+gigirassy@users.noreply.github.com>
2025-03-29 22:26:41 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import solid from "vite-plugin-solid";
 export default defineConfig({
 	plugins: [solid()],
 	base: "/",
+	build: {
+    	  outDir: "dist",  // Ensure this is the correct output directory
+  	}
 });
